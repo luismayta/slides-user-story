9e36604e93c41f89858308ee7e2f1bcba92a3339
@slovacus <slovacus@gmail.com>
2017-06-13 14:28:05 -0500

    !#2 Fix: clean script

--- script/clean.sh
+++ script/clean.sh
@@ -5,7 +5,7 @@
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 {
-    rm -rf -- **/**/*__pycache__
+    rm -rf -- **/**/*_pycache__
     rm -rf -- **/**/*.pyc
     rm -rf -- **/**/*.swp
 } >> /dev/null 2>&1
