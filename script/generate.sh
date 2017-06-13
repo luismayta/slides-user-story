eddb277d97ab3be5cbcd3b85372a9389d6fbc705
@slovacus <slovacus@gmail.com>
2017-06-13 14:03:53 -0500

    chore: implement script for makefile (#1)

---
+++ script/generate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# shellcheck source=/dev/null
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+cd "${SOURCE_DIR}" || echo 'path not found'
+
+landslide "${ETC_DIR}/"landslide.cfg
